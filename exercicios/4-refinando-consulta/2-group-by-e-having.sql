44e61d147f2bd4e7dcc6a654db92a2af32eb48ea
Veríssimo Silva <116843631+theverasdev@users.noreply.github.com>
2025-08-19 15:48:51 +0000

    Exercicios Concluídos

--- exercicios/4-refinando-consulta/2-group-by-e-having.sql
+++ exercicios/4-refinando-consulta/2-group-by-e-having.sql
@@ -1,7 +1,35 @@
 -- Crie uma consulta usando as tabelas "invoices" e "customers" cujo o resultado seja o total de gastos por cliente
 
+SELECT
+cus.CustomerId AS id_cliente,
+cus.FirstName AS nome,
+SUM(inv.Total) AS total_gasto
+FROM
+invoices AS inv
+INNER JOIN customers as cus ON inv.CustomerId = cus.CustomerId
+GROUP BY id_cliente;
 
 -- Na consulta anterior, insira um filtro para que apresente apenas os clientes que gastaram mais de R$30
 
+SELECT
+cus.CustomerId AS id_cliente,
+cus.FirstName AS nome,
+SUM(inv.Total) AS total_gasto
+FROM
+invoices AS inv
+INNER JOIN customers as cus ON inv.CustomerId = cus.CustomerId
+GROUP BY id_cliente
+HAVING SUM(inv.Total) >= 30;
 
 -- Na consulta anterior, insira uma coluna que informe a compra de maior valor feita por cada cliente
+
+SELECT
+cus.CustomerId AS id_cliente,
+cus.FirstName AS nome,
+SUM(inv.Total) AS total_gasto,
+COUNT(inv.Total) AS qnt_compras
+FROM
+invoices AS inv
+INNER JOIN customers as cus ON inv.CustomerId = cus.CustomerId
+GROUP BY id_cliente
+HAVING SUM(inv.Total) >= 30;
--- exercicios/4-refinando-consulta/3-ordenando-e-limitando.sql
+++ exercicios/4-refinando-consulta/3-ordenando-e-limitando.sql
@@ -1,7 +1,52 @@
 -- Desenvolva uma consulta para a tabela "customers" que retorne apenas 10 linhas
 
+SELECT
+CustomerId AS id,
+FirstName as nome,
+Country as pais
+FROM
+customers
+LIMIT 10;
+
 
 -- Usando a consulta anterior, ordene a coluna "Country" de forma crescente e "FirstName" de forma decrescente
 
 
+
+
+
+
+SELECT
+CustomerId AS id,
+FirstName as nome,
+Country as pais
+FROM
+customers
+ORDER BY
+pais ASC
+LIMIT 10;
+
+
+SELECT
+CustomerId AS id,
+FirstName as nome,
+Country as pais
+FROM
+customers
+ORDER BY
+pais ASC,
+nome DESC
+LIMIT 10;
+
 -- Continue usando a consulta anterior, dessa vez retorne 10 resultados, no entando ignore as 3 primeiras linhas
+
+SELECT
+CustomerId AS id,
+FirstName as nome,
+Country as pais
+FROM
+customers
+ORDER BY
+pais ASC,
+nome DESC
+LIMIT 10 OFFSET 3;
