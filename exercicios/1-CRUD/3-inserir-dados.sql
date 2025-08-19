9ce2f8a3d6fbe1769070603ec4155b443365cee8
Veríssimo Silva <116843631+theverasdev@users.noreply.github.com>
2025-08-19 14:25:44 +0000

    Exercicios Concluidos

--- exercicios/1-CRUD/3-inserir-dados.sql
+++ exercicios/1-CRUD/3-inserir-dados.sql
@@ -1,4 +1,15 @@
 -- Insira 3 classes salariais na tabela salario_classe
 
+INSERT INTO salario_classe(id_salario, salario, nivel)
+VALUES
+(1, 3500, 'estagiária'),
+(2, 4700, 'iniciante - grade 1'),
+(3, 5300, 'iniciante - grade 2');
+
 
 -- Insira 1 nova classe salarial, sem declarar o atributo id_salario
+
+INSERT INTO salario_classe(salario, nivel)
+VALUES
+(8500, 'gerencia'),
+(10700, 'coordenação de área');
--- exercicios/1-CRUD/6-remover-dados.sql
+++ exercicios/1-CRUD/6-remover-dados.sql
@@ -1,4 +1,14 @@
 -- Da tabela invoice, remova o invoice cujo o InvoiceID corresponda ao valor 33
 
+DELETE FROM tabela
+WHERE condicao;
+
+DELETE FROM invoices
+WHERE invoiceId = 33;
+
+
 
 -- Da tabela invoice, remova todos os invoices cujo o Total seja menor do que 1
+
+DELETE FROM invoices
+WHERE Total < 1;
