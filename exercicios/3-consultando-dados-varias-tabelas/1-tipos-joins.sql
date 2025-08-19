26d15b106144be6f5ac91c7b18880bbf0330930d
Veríssimo Silva <116843631+theverasdev@users.noreply.github.com>
2025-08-19 15:28:35 +0000

    Exercicios Concluidos

--- exercicios/3-consultando-dados-varias-tabelas/1-tipos-joins.sql
+++ exercicios/3-consultando-dados-varias-tabelas/1-tipos-joins.sql
@@ -1,1 +1,5 @@
 -- Crie uma consulta para realizar um left join com as tabelas "albums" e "artists"
+
+SELECT *
+FROM albums
+LEFT JOIN artists ON albums.ArtistId = artists.ArtistId;
--- exercicios/3-consultando-dados-varias-tabelas/2-join-e-with.sql
+++ exercicios/3-consultando-dados-varias-tabelas/2-join-e-with.sql
@@ -1,4 +1,33 @@
 -- Crie uma consulta para realizar inner join com as tabelas "tracks", "albums" e "artists"
 
+SELECT
+tra.TrackId AS id,
+tra.Name AS musica,
+alb.Title AS album,
+art.Name AS artista
+FROM
+tracks AS tra
+INNER JOIN albums AS alb ON tra.AlbumId = alb.AlbumId
+INNER JOIN artists AS art ON art.ArtistId = alb.ArtistId;
 
 -- Refatore a consulta anterior usando a cláusula "with" e verifique o total de músicas existente na base de terminado artista, ex: Caetano Veloso
+
+WITH musica AS 
+(
+  SELECT
+  tra.TrackId AS id,
+  tra.Name AS musica,
+  alb.Title AS album,
+  art.Name AS artista
+  FROM
+  tracks AS tra
+  INNER JOIN albums AS alb ON tra.AlbumId = alb.AlbumId
+  INNER JOIN artists AS art ON art.ArtistId = alb.ArtistId
+)
+SELECT
+artista,
+COUNT(musica) AS qnt_total_musicas
+FROM
+musica
+WHERE
+artista LIKE 'Caetano%'
